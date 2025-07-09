ac7e163568f154bcc253ab9a37d25f035551ea21
SuryaReddy-A <reddysurya413@gmail.com>
2025-07-09 16:02:59 +0530

    dom.js

--- .vscode/abc.js
+++ .vscode/abc.js
@@ -1,28 +1,19 @@
 function addTask(){
-    const newTask  =  document.createElement('li');
-    const taskList = document.getElementById('taskList');
-    newTask.textContent= document.getElementById('inputTask').value;
+    const newTask=document.createElement('li');
+    const taskList=document.getElementById('taskList');
+    newTask.textContent=document.getElementById('inputTask').value
     taskList.appendChild(newTask);
     document.getElementById('inputTask').value=""
     deleteTask(newTask)
-    deleteTask1(inputTask)
+
 }
 function deleteTask(newTask)
 {
-  const deleteBtn= document.createElement("button")
-        deleteBtn.textContent="Delete"
-        newTask.appendChild(deleteBtn)
-        deleteBtn.onclick=function(){
-            newTask.remove()
-        }
-}
-function deleteTask1(inputTask)
-{
-    const deleteBtn1=document.createElement("Button")
-          deleteBtn1.textContent="Delete"
-          inputTask.appendchild(deleteBtn1)
-          deleteBtn1.onclick=function(){
-          inputTask.remove()
-           
+    const deletebtn=document.createElement('button');
+          deletebtn.textContent="Delete";
+          deletebtn.classList.add("delete-btn");
+          newTask.appendChild(deletebtn)
+          deletebtn.onclick=function(){
+            newTask.remove();
           }
 }
