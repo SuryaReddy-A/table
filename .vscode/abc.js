b9ccc3f69e5b63a60864b12a90eb4d7a73dd032b
SuryaReddy-A <reddysurya413@gmail.com>
2025-07-09 08:39:28 +0530

    DOM

--- .vscode/abc.js
+++ .vscode/abc.js
@@ -1,1 +1,28 @@
-l
+function addTask(){
+    const newTask  =  document.createElement('li');
+    const taskList = document.getElementById('taskList');
+    newTask.textContent= document.getElementById('inputTask').value;
+    taskList.appendChild(newTask);
+    document.getElementById('inputTask').value=""
+    deleteTask(newTask)
+    deleteTask1(inputTask)
+}
+function deleteTask(newTask)
+{
+  const deleteBtn= document.createElement("button")
+        deleteBtn.textContent="Delete"
+        newTask.appendChild(deleteBtn)
+        deleteBtn.onclick=function(){
+            newTask.remove()
+        }
+}
+function deleteTask1(inputTask)
+{
+    const deleteBtn1=document.createElement("Button")
+          deleteBtn1.textContent="Delete"
+          inputTask.appendchild(deleteBtn1)
+          deleteBtn1.onclick=function(){
+          inputTask.remove()
+           
+          }
+}
